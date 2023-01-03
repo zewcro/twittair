d64d7a2fcd4236f986c76c27ff99719790084a9c
zewcro <theorousseau75@gmail.com>
2023-01-04 00:53:36 +0100

    some updates

--- frontend/src/Components/One_Twit.js
+++ frontend/src/Components/One_Twit.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "../Styles/OneTwit.css";
+import LikeButton from "./LikeButton";
+
+
 
 function OneTwit({ id, author,author_profil_pic, content, like, rt, publication }) {
+
+
+
   return (
     <>
       <link
@@ -63,22 +69,7 @@ function OneTwit({ id, author,author_profil_pic, content, like, rt, publication 
             <div className="retweet-count">{rt}</div>
           </div>
 
-          <div className="likes">
-            <svg
-              className="feather feather-heart sc-dnqmqq jxshSx"
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              aria-hidden="true"
-            >
-              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-            </svg>
+         <LikeButton />
             <div className="likes-count">{like}</div>
           </div>
 
@@ -101,7 +92,6 @@ function OneTwit({ id, author,author_profil_pic, content, like, rt, publication 
             </svg>
           </div>
         </div>
-      </div>
     </>
   );
 }
